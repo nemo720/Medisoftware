ef95d4d6baedb7eaedd664861d151df73af12027
nemo720 <quandoan.140203@gmail.com>
2024-03-03 18:56:36 +0700

    CodeFE

---
+++ FEmedicine_sales_management/src/user/employee/doanh_thu_day/DanhThuMonth.js
@@ -0,0 +1,91 @@
+import {format} from 'date-fns'
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+export function DanhThuMonth(){
+    const [collects,setCollects] = useState();
+    const [spends,setSpends] = useState();
+    useEffect(() => {
+        findAllCollects();
+        findAllSpends();
+    }, []);
+    const findAllCollects = async () => {
+        try{
+            let temp = await axios.get("http://localhost:8080/api/bill/list");
+            setCollects(temp.data);
+        }catch (e){
+            console.log(e);
+        }
+    }
+    const findAllSpends = async () => {
+        try{
+            let temp = await axios.get("http://localhost:8080/api/inputWarehouse/list");
+            setSpends(temp.data);
+        }catch (e){
+            console.log(e);
+        }
+    }
+
+    if(!spends || !collects) return null;
+
+// format the number
+    const formatCurrency = (value) =>
+        new Intl.NumberFormat('en', { style: 'currency', currency: 'VND' }).format(
+            value
+        );
+
+
+    // format data
+
+    const formatCollects = collects.map(collect => {
+        return {
+            date: format(collect.dateCreate, 'dd/MM/yyyy'),
+            totalPrice: collect.totalPayment,
+            totalPayment: 0
+        }
+    })
+
+    const formatSpends = spends.map(spend=>{
+        return{
+            date: format(spend.inputDay, 'dd/MM/yyyy'),
+            totalPayment: spend.totalPayment,
+            totalPrice: 0
+        }
+    })
+
+    const formatData = formatCollects.concat(formatSpends).reduce((acc, cur)=>{
+        const key = cur.date;
+        if(acc[key]){
+            acc[key] = {...acc[key], totalPrice:  Math.ceil(acc[key].totalPrice + cur.totalPrice), totalPayment: Math.ceil(acc[key].totalPayment + cur.totalPayment)}
+        }else{
+            acc[key] = {date: key, totalPrice: cur.totalPrice, totalPayment: cur.totalPayment}
+        }
+        return acc;
+    }, {})
+    // sort date
+    const data = Object.values(formatData).sort((a,b)=>{
+        a = a.date.split('/').reverse().join('');
+        b = b.date.split('/').reverse().join('');
+        return a > b ? 1 : a < b ? -1 : 0 ;   // old first
+        // return a < b ? 1 : a > b ? -1 : 0 ; // new first
+    })
+    return (
+        <>
+            <div style={{padding: '100px'}}>
+                <ResponsiveContainer width='100%' height={500}>
+                    <BarChart data={data}>
+                        <CartesianGrid strokeDasharray="4 4" />
+                        <Legend/>
+                        <XAxis dataKey='date'/>
+                        <YAxis  tickFormatter={(value)=> `${value}`}/>
+                        <Tooltip/>
+                        <Bar dataKey='totalPrice' fill="#8884d8"/>
+                        <Bar dataKey='totalPayment' fill="#82ca9d"/>
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
+
+        </>
+    )
+}
